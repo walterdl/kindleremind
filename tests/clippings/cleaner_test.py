067012caecebfabde5eb60cd532613703b37c65e
Walter Devia <walter.devia@onevisionresources.com>
2023-06-22 20:48:22 -0500

    clippings.cleaner tests.

---
+++ tests/clippings/cleaner_test.py
@@ -0,0 +1,37 @@
+import clippings.cleaner as cleaner
+
+
+def test_remove_initial_white_spaces():
+    line = '    hello-world'
+    result = cleaner.clear_line(line)
+    assert result == 'hello-world'
+
+
+def test_remove_initial_break_lines():
+    line = '\n\n\n\n\nhello-world'
+    result = cleaner.clear_line(line)
+    assert result == 'hello-world'
+
+
+def test_remove_initial_boom_chars():
+    line = f'{chr(65279)}{chr(65279)}{chr(65279)}hello-world'
+    result = cleaner.clear_line(line)
+    assert result == 'hello-world'
+
+
+def test_remove_final_white_spaces():
+    line = 'hello-world    '
+    result = cleaner.clear_line(line)
+    assert result == 'hello-world'
+
+
+def test_remove_final_break_lines():
+    line = 'hello-world\n\n\n\n\n'
+    result = cleaner.clear_line(line)
+    assert result == 'hello-world'
+
+
+def test_remove_final_boom_chars():
+    line = f'hello-world{chr(65279)}{chr(65279)}{chr(65279)}'
+    result = cleaner.clear_line(line)
+    assert result == 'hello-world'
--- tests/one_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import clippings.metadata as metadata
-
-
-def test_hello():
-    print('The sys.path', sys.path)
-    print(metadata._BOOKMARK_TEXT)
-    assert (1, 2) == (1, 2)
