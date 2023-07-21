c89eae17fc46391f923269082a504a79d09c59ff
Walter Devia <walter.devia@onevisionresources.com>
2023-07-20 22:43:52 -0500

    Move lib out.

--- server/src/lib/mongodb/connection.py
+++ server/src/lib/mongodb/connection.py
@@ -1,13 +1,6 @@
-import pymongo
+from lib.config import config
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from lib.config import config
 
-print('Connecting to mongodb')
 client = MongoClient(config.mongodb_uri, server_api=ServerApi('1'))
 clippings = client.kindleremind.clippings
-
-print('Creating clipping indexes')
-clippings.create_index([("key", pymongo.ASCENDING)], unique=True)
-
-print('Done')
