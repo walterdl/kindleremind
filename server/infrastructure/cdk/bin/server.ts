ba009d1e70359925b4ba1498a65ce8120fe5b43f
Walter Devia <walter.devia@onevisionresources.com>
2023-07-29 09:31:26 -0500

    Remove AccessesStack.

--- server/infrastructure/cdk/bin/server.ts
+++ server/infrastructure/cdk/bin/server.ts
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { AccessesStack } from "../lib/accesses-stack";
-import { ServerStack } from "../lib/server-stack";
+import { Stack } from "../lib/stack";
 
 const app = new cdk.App();
-const accessesStack = new AccessesStack(app, "AccessesStack");
-const serverStack = new ServerStack(app, "ServerStack", {});
-serverStack.addDependency(accessesStack);
+new Stack(app, "ServerStack", {});
