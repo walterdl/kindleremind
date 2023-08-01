4e83b36d8b87755bf0aabff253f9c3d29abc2532
Walter Devia <walter.devia@onevisionresources.com>
2023-07-31 21:21:31 -0500

    CDK endpoint.

--- server/infrastructure/cdk/lib/stack.ts
+++ server/infrastructure/cdk/lib/stack.ts
@@ -1,16 +1,41 @@
+import path = require("path");
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const saveClippingsFunction = new lambda.Function(
+      this,
+      "SaveClippingsFunction",
+      {
+        runtime: lambda.Runtime.PYTHON_3_11,
+        handler: "api/post_clippings/handler.lambda_handler",
+        code: lambda.Code.fromAsset(path.resolve(__dirname, "../../../src")),
+        functionName: "SaveClippingsFunction",
+        timeout: cdk.Duration.minutes(10),
+      }
+    );
+    const saveClippingsIntegration = new HttpLambdaIntegration(
+      "SaveClippingsIntegration",
+      saveClippingsFunction
+    );
+    const httpApi = new apigwv2.HttpApi(this, "HttpApi", {
+      createDefaultStage: false,
+      disableExecuteApiEndpoint: false,
+    });
+    new apigwv2.HttpStage(this, "Stage", {
+      httpApi,
+      stageName: "v1",
+    });
+    httpApi.addRoutes({
+      path: "/clippings",
+      methods: [apigwv2.HttpMethod.POST],
+      integration: saveClippingsIntegration,
+    });
   }
 }
