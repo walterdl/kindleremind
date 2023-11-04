9618b3f81b8954152cedf71f40fc984cda241ff2
Walter Devia <walter.devia@onevisionresources.com>
2023-11-04 09:42:53 -0500

    Fix wrong fixture import.

--- server/tests/api/delete_api_key/storage_test.py
+++ server/tests/api/delete_api_key/storage_test.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 from bson.objectid import ObjectId
 
 from kindleremind.api.delete_api_key.storage import ApiKeyStorage
-from tests.api.common_fixtures import app_context, doc_id
+from tests.api.common_fixtures import app_context, doc_id, doc_bid
 
 
 @pytest.fixture()
@@ -11,14 +11,13 @@ def instance(app_context):
     return ApiKeyStorage(app_context, Mock())
 
 
-def test_delete_api_key_by_given_key(instance, api_key_id):
-    instance.delete_api_key(api_key_id)
+def test_delete_api_key_by_given_key(instance, doc_id, doc_bid):
+    instance.delete_api_key(doc_id)
 
-    assert instance.collection.delete_one.call_args.args[0]['_id'] == ObjectId(
-        api_key_id)
+    assert instance.collection.delete_one.call_args.args[0]['_id'] == doc_bid
 
 
-def test_delete_api_key_using_user_context(instance, app_context, api_key_id):
-    instance.delete_api_key(api_key_id)
+def test_delete_api_key_using_user_context(instance, app_context, doc_id):
+    instance.delete_api_key(doc_id)
 
     assert instance.collection.delete_one.call_args.args[0]['user'] == app_context['email']
