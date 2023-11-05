af7c5a72c2bca3e3783f68523d349cee1cc86e05
Walter Devia <walter.devia@onevisionresources.com>
2023-11-04 20:25:54 -0500

    Small typo.

--- server/src/kindleremind/api/api_key_authorizer/factory.py
+++ server/src/kindleremind/api/api_key_authorizer/factory.py
@@ -4,7 +4,7 @@ from kindleremind.lib.mongodb.connection import api_keys
 
 
 def get_service():
-    # No context because the service is ran from a Lambda authorizer.
+    # No context because the service is run by a Lambda authorizer.
     # And the Lambda authorizers is actually the context initializer.
     no_context = {}
     return ApiKeyAuthorizer(Storage(no_context, api_keys))
