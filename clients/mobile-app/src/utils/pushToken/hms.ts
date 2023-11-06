d7007edcdc76dd4b66f72fe4b2f9c4328ddc1acd
Walter Devia <walter.devia@onevisionresources.com>
2023-11-06 13:50:26 -0500

    Deprecate HMS and upgrade RN.

--- clients/mobile-app/src/utils/pushToken/hms.ts
+++ clients/mobile-app/src/utils/pushToken/hms.ts
@@ -1,60 +1,9 @@
-import {useEffect} from 'react';
-import {HmsPushInstanceId, HmsPushEvent} from '@hmscore/react-native-hms-push';
+import {UseInitialPushToken, UsePushTokenChanges} from './types';
 
-import {
-  UseInitialPushToken,
-  UsePushTokenChanges,
-  PushTokenProto,
-} from './types';
-
-export const useInitialHmsPushToken: UseInitialPushToken = receivePushToken => {
-  useEffect(() => {
-    HmsPushInstanceId.getToken('')
-      .then((rawResponse: Object) => {
-        // Built-in type from HMS specifies the response as Object. Thus, it needs casting.
-        const response = rawResponse as unknown as HmsPushTokenResponse;
-        if (response?.result?.length) {
-          receivePushToken(response.result, PushTokenProto.HMS);
-        }
-      })
-      .catch(error => {
-        console.error('Error receiving HMS Push Token', error);
-      });
-  }, [receivePushToken]);
+export const useInitialHmsPushToken: UseInitialPushToken = () => {
+  throw new Error('Not implemented');
 };
 
-// HMS Push has incorrect type for this function. Thus, it needs casting.
-const onTokenReceived =
-  HmsPushEvent.onTokenReceived as unknown as OnTokenReceived;
-
-export const useHmsPushTokenChanges: UsePushTokenChanges = receivePushToken => {
-  useEffect(() => {
-    const subscription = onTokenReceived(response => {
-      console.log('OnTokenReceived', response);
-      if (response?.token?.length) {
-        receivePushToken(response.token, PushTokenProto.HMS);
-      }
-    });
-
-    return () => {
-      subscription.remove();
-    };
-  }, [receivePushToken]);
-};
-
-interface HmsPushTokenResponse {
-  result?: string;
-  resultCode: string;
-}
-
-type OnTokenReceived = (
-  callback: (response?: OnTokenReceivedResponse) => void,
-) => OnTokenReceivedSubscription;
-
-interface OnTokenReceivedResponse {
-  token?: string;
-}
-
-type OnTokenReceivedSubscription = {
-  remove: () => void;
+export const useHmsPushTokenChanges: UsePushTokenChanges = () => {
+  throw new Error('Not implemented');
 };
