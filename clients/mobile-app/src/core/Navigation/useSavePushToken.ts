cb5c64bc892d6e441a92a1276e4b9e8733608f8d
Walter Devia <walter.devia@onevisionresources.com>
2023-11-11 20:53:00 -0500

    Fix path to post push token.

--- clients/mobile-app/src/core/Navigation/useSavePushToken.ts
+++ clients/mobile-app/src/core/Navigation/useSavePushToken.ts
@@ -1,10 +1,7 @@
 import {useCallback} from 'react';
-import Config from 'react-native-config';
 
 import {useFetch} from '../../utils/useFetch';
 import {useInitialPushToken, usePushTokenChanges} from '../../utils/pushToken';
-
-const POST_TOKEN_URL = Config.API_URL + '/push-token';
 
 export function useSavePushToken() {
   const postPushToken = usePostPushToken();
@@ -17,7 +14,7 @@ function usePostPushToken() {
 
   return useCallback(
     async (pushToken: string, proto: string) => {
-      await fetch(POST_TOKEN_URL, {
+      await fetch('/push-token', {
         method: 'POST',
         body: JSON.stringify({pushToken, proto}),
         headers: {
