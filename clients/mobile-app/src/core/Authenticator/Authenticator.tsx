865244b3b4440849e68e211a8ab4bd1ec297cffb
Walter Devia <walter.devia@onevisionresources.com>
2023-12-11 15:13:45 -0500

    Logo in authenticator.

--- clients/mobile-app/src/core/Authenticator/Authenticator.tsx
+++ clients/mobile-app/src/core/Authenticator/Authenticator.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import {Image} from 'react-native';
 import {Amplify} from 'aws-amplify';
 import {
   Authenticator,
   ThemeProvider as AuthThemeProvider,
 } from '@aws-amplify/ui-react-native';
 import {DefaultSignUpProps} from '@aws-amplify/ui-react-native/dist/Authenticator/Defaults/types';
+import {makeStyles} from '@rneui/themed';
 
 import {awsExports} from './awsExports';
 import {theme} from './theme';
 import {signUpFields} from './signUpFields';
+import {View} from 'react-native';
 
 Amplify.configure(awsExports);
 
@@ -19,6 +22,7 @@ export function AppAuthenticator({children}: Props) {
         <Authenticator
           loginMechanisms={['email']}
           signUpAttributes={['email', 'given_name']}
+          Header={Header}
           components={{
             SignUp,
           }}>
@@ -26,6 +30,27 @@ export function AppAuthenticator({children}: Props) {
         </Authenticator>
       </Authenticator.Provider>
     </AuthThemeProvider>
+  );
+}
+
+const headerStyles = makeStyles(() => ({
+  container: {
+    width: '100%',
+    height: 125,
+  },
+  img: {
+    width: '100%',
+    flex: 1,
+    resizeMode: 'contain',
+  },
+}));
+
+function Header() {
+  const styles = headerStyles();
+  return (
+    <View style={styles.container}>
+      <Image source={require('./logo.png')} style={styles.img} />
+    </View>
   );
 }
 
