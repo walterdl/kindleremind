131c2028daadf016e8fa9a993ffc0a972adb2d28
Walter Devia <walter.devia@onevisionresources.com>
2023-12-12 15:32:03 -0500

    Make clippings and schedules collections timezone aware.

--- server/src/kindleremind/lib/mongodb/connection.py
+++ server/src/kindleremind/lib/mongodb/connection.py
@@ -1,9 +1,16 @@
+from bson import CodecOptions
 from kindleremind.lib.config import get_config
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 client = MongoClient(get_config('mongodb_uri'), server_api=ServerApi('1'))
-clippings = client.kindleremind.clippings
+
+clippings = client.kindleremind.get_collection(
+    "clippings", codec_options=CodecOptions(tz_aware=True))
+
 pushtokens = client.kindleremind.pushtokens
+
 api_keys = client.kindleremind.apikeys
-schedules = client.kindleremind.schedules
+
+schedules = client.kindleremind.get_collection(
+    "schedules", codec_options=CodecOptions(tz_aware=True))
